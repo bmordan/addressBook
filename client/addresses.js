6dfe9015a1b51cfeb92425fe8623d24df46784b1
Bernard Mordan <bernardmordan@gmail.com>
2015-01-13 12:06:27 +0000

    simple sequential layout

--- client/addresses.js
+++ client/addresses.js
@@ -1,21 +1,29 @@
 famous.polyfills
 famous.core.famous
-
 UI.body.rendered = function(){
   var context = new famous.core.Engine.createContext()
-  var surface = new famous.core.Surface({
-    size: [200, 60],
-    content: "Hello You",
-    properties: {
-      backgroundColor: "hotPink",
-      color: "white",
-      textAlign: "center",
-      lineHeight: "60px"
-    }
+  var sequentialLayout = new famous.views.SequentialLayout
+  var cards = Contacts.find({})
+  var addressCards = []
+  var counter = 0
+  sequentialLayout.sequenceFrom(addressCards)
+  _.each(cards, function(card){
+    var surface = new famous.core.Surface({
+      size: [200,60],
+      content: counter+=1,
+      properties: {
+        backgroundColor: "rgb(36,36,196)",
+        paddingLeft: "1%",
+        lineHeight: "60px",
+        border: "solid 2px white",
+        color: "white"
+      }
+    })
+    addressCards.push(surface)
   })
-  var modifier = new famous.core.Modifier({
-    origin: [0.5, 0.5],
-    align: [0.5, 0.5]
+  var sequentialLayoutModifier = new famous.core.Modifier({
+    origin: [0.5, 0],
+    align: [0.5, 0]
   })
-  context.add(modifier).add(surface)
+  context.add(sequentialLayoutModifier).add(sequentialLayout)
 }
--- data/contacts.js
+++ data/contacts.js
@@ -1,1 +1,4 @@
 Contacts = new Mongo.Collection('contacts')
+Contacts.getAll = function(){
+  return Contact.find({}).fetch()
+}
